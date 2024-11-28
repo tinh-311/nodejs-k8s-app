cb9b9e2bbb75ee9065ea5ff00f85bed69079e9aa
Tom Le <tom.l@itcgroup.io>
2024-11-28 14:48:35 +0700

    Init

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js on Kubernetes! 💝💝💝");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
