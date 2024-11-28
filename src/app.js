c483a1f61bf8b115cecb632ff0fee6ca79f8803f
Tom Le <tom.l@itcgroup.io>
2024-11-28 14:59:36 +0700

    Change

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js on Kubernetes! 💝💝💝");
+  res.send("Hello from Node.js on Kubernetes!");
 });
 
 app.listen(port, () => {
